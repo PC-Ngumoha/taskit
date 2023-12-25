cb4bd6a75eaa47aa5f6d5f20fa2a771a0d5a9123
PC-Ngumoha <pcngumoha@gmail.com>
2023-12-25 05:21:39 +0100

    Setup: Setup the Django project

---
+++ taskit_api/common/models.py
@@ -0,0 +1,13 @@
+"""
+Contains the definition of a model to abstract away all attributes shared
+in common by all other models in the app
+"""
+from django.db import models
+
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('created_at',)
